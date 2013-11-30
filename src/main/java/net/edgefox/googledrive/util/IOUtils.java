53c48d91ba7038979538cf7a08136add337aecbd
Ivan Lyutov <ilyutov@cogniance.com>
2013-11-30 15:05:45 +0200

    Added util method for file md5 checksum retrieval

--- src/main/java/net/edgefox/googledrive/util/IOUtils.java
+++ src/main/java/net/edgefox/googledrive/util/IOUtils.java
@@ -1,10 +1,20 @@
 package net.edgefox.googledrive.util;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 /**
  * User: Ivan Lyutov
@@ -34,5 +44,15 @@ public class IOUtils {
         } catch (IOException e) {
             logger.error("Unable to close the stream", e);
         }
+    }
+    
+    public static String getFileMd5CheckSum(Path path) throws NoSuchAlgorithmException, IOException {
+        byte[] digestBytes = DigestUtils.md5(Files.readAllBytes(path));
+        StringBuilder sb = new StringBuilder("");
+        for (byte digestByte : digestBytes) {
+            sb.append(Integer.toString((digestByte & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
     }
 }
